33843e7bb9d3e1f73e2575dd2f9e56232db1d600
trilusa <adriansalustri@gmail.com>
2024-04-07 15:44:36 -0600

    Improved ITF

--- Algorithms/ITD/ITD_RT.py
+++ Algorithms/ITD/ITD_RT.py
@@ -60,11 +60,9 @@ for i in range(int(RATE / CHUNK * 1),len(frames)):
     max_val = 2**15
     left_channel_normalized = left_channel_float / max_val
     right_channel_normalized = right_channel_float / max_val
-    # L.append(left_channel_normalized)
-    # R.append(right_channel_normalized)
+    L.append(left_channel_normalized)
+    R.append(right_channel_normalized)
     detected, power = signal_detector(left_channel_normalized, thresh=.01)
-        
-    # Perform correlation
     if(detected):
         print(detected, power)
         L.append(left_channel_normalized)
@@ -72,32 +70,54 @@ for i in range(int(RATE / CHUNK * 1),len(frames)):
         corr.append(
             signal.correlate(left_channel_normalized, right_channel_normalized, mode='same') / len(left_channel_normalized)
         )
-    else:
-        L.append(np.zeros(np.size(left_channel_normalized)))
-        R.append(np.zeros(np.size(right_channel_normalized)))
+L = np.concatenate(L)
+R = np.concatenate(R)
+
+segment_length = CHUNK
+overlap = CHUNK/2
+step = segment_length - overlap
+num_segments = int(np.ceil((len(L) - overlap) / step))
+spectrogram = np.zeros((int(segment_length / 2 + 1), num_segments))
+
+for i in range(num_segments):
+    start = int(i * step)
+    end = int(start + segment_length)
+    segment = L[start:end]
+    if len(segment) < segment_length:
+        segment  = np.pad(segment, (0, segment_length - len(segment)), 'constant')
+    windowed_segment = segment * np.hamming(segment_length)
+    fft_result = np.fft.rfft(windowed_segment)
+    power_spectrum = np.abs(fft_result) ** 2
+    spectrogram[:, i] = power_spectrum
+freqs = np.fft.rfftfreq(segment_length, 1/RATE)
+times = np.arange(num_segments) * step / RATE
+
+    # Perform correlation
+    
 # print(len(corr))
 corr_mean = np.mean(corr,axis=0)
 samp_delay_est = np.argmax(corr_mean)-CHUNK/2
 sec_delay_est = samp_delay_est / RATE
 meter_delay_est = sec_delay_est * 343.0 #assume speed of sound
 cm_delay_est = meter_delay_est*100
-print(f"{samp_delay_est}samples, {sec_delay_est*1e6}us, {cm_delay_est}cm")
 print(len(corr), corr_mean)
+print(f"{samp_delay_est}samples, {sec_delay_est*1e6}us, {cm_delay_est}cm")
+
 
 # pr.disable()  # Stop collecting profiling data
 # ps = pstats.Stats(pr).sort_stats('cumulative')  # Create Stats object
 # ps.print_stats()  # Print the statistics
 
 #Plotting
-r=3
+r=4
 c=1
-plt.figure(figsize=(10,4))
+plt.figure(figsize=(16,12))
 plt.subplot(r,c,1)
-plt.plot(np.concatenate(L), label="left")
-plt.plot(np.concatenate(R), label="Right")
+plt.plot(L, label="left")
+plt.plot(R, label="Right")
 plt.legend(loc='right')
 # plt.subplot(r,c,2)
-plt.title(f"Audio Recording for {len(np.concatenate(R))/RATE} seconds ({len(np.concatenate(R))} samples)")
+plt.title(f"Audio Recording for {len(R)/RATE} seconds ({len(R)} samples)")
 plt.xlabel('Sample Number')
 plt.ylabel('Amplitude')
 
@@ -108,8 +128,14 @@ for i in range(0,len(corr)):
 plt.subplot(r,c,3)
 plt.title(f"Correlation. sample delay estimate={samp_delay_est} samples, distance = {cm_delay_est}cm")
 plt.plot(range(-CHUNK//2, CHUNK//2),corr_mean)
-plt.show()
 
+plt.subplot(r,c,4)
+plt.pcolormesh(times, freqs, 10 * np.log10(spectrogram), shading='gouraud')
+plt.ylabel('Frequency [Hz]')
+plt.xlabel('Time [sec]')
+plt.title('Spectrogram')
+plt.colorbar(label='Intensity [AU]')
+plt.show()
 
 # wf = wave.open(OUTPUT_FILENAME, 'wb')
 # wf.setnchannels(CHANNELS)
